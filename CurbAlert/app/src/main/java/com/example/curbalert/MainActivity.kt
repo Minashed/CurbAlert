8d451293322e5a2740189608bffb85c036768dde
Fady Andraws <fady.andraws@mail.utoronto.ca>
2020-09-28 16:50:35 -0400

    added some code (this is just a test)

--- CurbAlert/app/src/main/java/com/example/curbalert/MainActivity.kt
+++ CurbAlert/app/src/main/java/com/example/curbalert/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.curbalert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // TODO page navigations
+
+        val gson = GsonBuilder().create() //attempting to use a new dependency for json
     }
 }
