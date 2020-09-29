8c205c50b9ae2c159418b01df2028ccc656d8888
Fady Andraws <fady.andraws@mail.utoronto.ca>
2020-09-29 09:16:12 -0400

    user login logo constraints
    - got started on the user login

--- CurbAlert/app/src/main/java/com/example/curbalert/MainActivity.kt
+++ CurbAlert/app/src/main/java/com/example/curbalert/MainActivity.kt
@@ -5,12 +5,10 @@ import android.os.Bundle
 import com.google.gson.GsonBuilder
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // TODO page navigations
-
         val gson = GsonBuilder().create() //attempting to use a new dependency for json
     }
 }
